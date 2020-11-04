b1d133a047442251300763d79957cbe145a170ea
JoonKi-Kim <joonki402@gmail.com>
2020-11-04 17:15:35 +0900

    .

--- src/week01/WedIndienName.java
+++ src/week01/WedIndienName.java
@@ -6,7 +6,7 @@ public class WedIndienName {
     public static int[] getBirth() {
         int ret[] = new int[3];
         Scanner sc = new Scanner(System.in);
-        System.out.print("출생년도 끝자리:");
+        System.out.print("출생 년도:");
         ret [0] = sc.nextInt();
         System.out.print("출생 달:");
         ret [1] = sc.nextInt();
@@ -20,32 +20,31 @@ public class WedIndienName {
     public static void main(String[] args) {
 
         int[] arr = getBirth();
-        String year = String.valueOf(arr[0]);
+        String year = String.valueOf(arr[0]%10);
         String month = String.valueOf(arr[1]);
         String day = String.valueOf(arr[2]);
 
-        //StringBuffer sb = new StringBuffer();
         FileReader readFile;
         BufferedReader br;
         String getLine;
         try {
 
             readFile = new FileReader("C:\\Users\\jk2018\\Desktop\\cocoa\\src\\week01\\IndienName.txt");		// 파일경로
-
             br = new BufferedReader(readFile);
             String result = "";
 
-
-            while((getLine = br.readLine()) != null){		// 한줄씩 읽기 (마지막줄 체크)
+            while((getLine = br.readLine()) != null){		// 한줄씩 읽기
                 //System.out.println(getLine);
                 int idx = getLine.indexOf(":");
-                if(getLine.startsWith(day+"일")){			// 파일중 startsWith
-                    result +=  getLine.substring(idx+1);
+                if (Integer.parseInt(day) !=4 &&Integer.parseInt(day) !=5 && Integer.parseInt(day) !=6) {
+                    if(getLine.startsWith(day+"일")){
+                        result += getLine.substring(idx+1);
+                    }
                 }
-                if(getLine.startsWith(month+"월")){			// 파일중 startsWith
+                if(getLine.startsWith(month+"월")){
                     result += " " + getLine.substring(idx+1);
                 }
-                if(getLine.startsWith(year+"년")){			// 파일중 startsWith
+                if(getLine.startsWith(year+"년")){
                     result += getLine.substring(idx+1);
                 }
 
